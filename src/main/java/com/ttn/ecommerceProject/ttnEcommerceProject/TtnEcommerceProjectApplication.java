7b786077d3aebcba619544dc67a08c1c3abdce79
sakshi2-blip <sakshi2@tothenew.com>
2026-03-02 15:47:17 +0530

    Ecommerce Project

---
+++ src/main/java/com/ttn/ecommerceProject/ttnEcommerceProject/TtnEcommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ttn.ecommerceProject.ttnEcommerceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtnEcommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtnEcommerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttn/ecommerceProject/ttnEcommerceProject/TtnEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.ecommerceProject.ttnEcommerceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtnEcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
